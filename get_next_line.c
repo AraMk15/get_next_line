cf2c8e46e1b1f7f45324f02ab00d97a2b9ae2d43
Ara Mkrtchyan <50145647+AraMk15@users.noreply.github.com>
2025-04-17 15:28:54 +0400

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,21 @@
+#include "get_next_line.h"
+
+char *ft_read_file(int fd, char *buffer)
+{
+}
+
 char *get_next_line(int fd)
 {
     static char *buffer;
     char *line;
-    
-    
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+    {
+        free(buffer);
+        buffer = NULL;
+        return (NULL);
+    }
+    buffer = ft_read_file(fd, buffer);
+    if(!buffer)
+        return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE = 42
+# define BUFFER_SIZE  42
 
-# include <cstddef>
+# include <unistd.h>
 # include <fcntl.h>
 
 void ft_putchr(int c);
